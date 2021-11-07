cff5e698ab87b2b1bb7fadf6cdb4f2c3ab5db28a
tarokimuraorg <tarokimuraorg@outlook.com>
2021-11-07 23:19:05 +0900

    first commit

---
+++ Scale.swift
@@ -0,0 +1,72 @@
+//
+//  Scale.swift
+//  GhostWriter
+//
+//  Created by Taro Kimura on 2021/11/07.
+//
+
+import Foundation
+
+enum Scale {
+    
+    enum Major {
+        case Cmajor
+        case CSmajor
+        case Dmajor
+        case EFmajor
+        case Emajor
+        case Fmajor
+        case FSmajor
+        case Gmajor
+        case AFmajor
+        case Amajor
+        case BFmajor
+        case Bmajor
+    }
+
+    enum NaturalMinor{
+        case Cminor
+        case CSminor
+        case Dminor
+        case EFminor
+        case Eminor
+        case Fminor
+        case FSminor
+        case Gminor
+        case AFminor
+        case Aminor
+        case BFminor
+        case Bminor
+    }
+
+    enum HarmonicMinor {
+        case Cminor
+        case CSminor
+        case Dminor
+        case EFminor
+        case Eminor
+        case Fminor
+        case FSminor
+        case Gminor
+        case AFminor
+        case Aminor
+        case BFminor
+        case Bminor
+    }
+
+    enum MelodicMinor {
+        case Cminor
+        case CSminor
+        case Dminor
+        case EFminor
+        case Eminor
+        case Fminor
+        case FSminor
+        case Gminor
+        case AFminor
+        case Aminor
+        case BFminor
+        case Bminor
+    }
+    
+}
