46b3bf07f1f01847570a9ad4cbb020c84e1947e6
tarokimuraorg <tarokimuraorg@outlook.com>
2021-11-13 18:58:38 +0900

    素値を追加

--- Scale.swift
+++ Scale.swift
@@ -9,64 +9,72 @@ import Foundation
 
 enum Scale {
     
-    enum Major {
-        case Cmajor
-        case CSmajor
-        case Dmajor
-        case EFmajor
-        case Emajor
-        case Fmajor
-        case FSmajor
-        case Gmajor
-        case AFmajor
-        case Amajor
-        case BFmajor
-        case Bmajor
+    enum Major : UInt8 {
+        
+        case Cmajor = 1
+        case CSmajor = 2
+        case Dmajor = 3
+        case EFmajor = 4
+        case Emajor = 5
+        case Fmajor = 6
+        case FSmajor = 7
+        case Gmajor = 8
+        case AFmajor = 9
+        case Amajor = 10
+        case BFmajor = 11
+        case Bmajor = 12
+        
     }
 
-    enum NaturalMinor{
-        case Cminor
-        case CSminor
-        case Dminor
-        case EFminor
-        case Eminor
-        case Fminor
-        case FSminor
-        case Gminor
-        case AFminor
-        case Aminor
-        case BFminor
-        case Bminor
+    enum NaturalMinor : UInt8 {
+        
+        case Cminor = 13
+        case CSminor = 14
+        case Dminor = 15
+        case EFminor = 16
+        case Eminor = 17
+        case Fminor = 18
+        case FSminor = 19
+        case Gminor = 20
+        case AFminor = 21
+        case Aminor = 22
+        case BFminor = 23
+        case Bminor = 24
+        
     }
 
-    enum HarmonicMinor {
-        case Cminor
-        case CSminor
-        case Dminor
-        case EFminor
-        case Eminor
-        case Fminor
-        case FSminor
-        case Gminor
-        case AFminor
-        case Aminor
-        case BFminor
-        case Bminor
+    enum HarmonicMinor : UInt8 {
+        
+        case Cminor = 25
+        case CSminor = 26
+        case Dminor = 27
+        case EFminor = 28
+        case Eminor = 29
+        case Fminor = 30
+        case FSminor = 31
+        case Gminor = 32
+        case AFminor = 33
+        case Aminor = 34
+        case BFminor = 35
+        case Bminor = 36
+        
     }
 
-    enum MelodicMinor {
-        case Cminor
-        case CSminor
-        case Dminor
-        case EFminor
-        case Eminor
-        case Fminor
-        case FSminor
-        case Gminor
-        case AFminor
-        case Aminor
-        case BFminor
-        case Bminor
+    enum MelodicMinor : UInt8 {
+        
+        case Cminor = 37
+        case CSminor = 38
+        case Dminor = 39
+        case EFminor = 40
+        case Eminor = 41
+        case Fminor = 42
+        case FSminor = 43
+        case Gminor = 44
+        case AFminor = 45
+        case Aminor = 46
+        case BFminor = 47
+        case Bminor = 48
+        
     }
     
 }
