28576c7c0fb0f006960453d66b4a690e2b3b7965
tarokimuraorg <tarokimuraorg@outlook.com>
2021-12-02 21:08:42 +0900

    マイナースケールからコードを取得する機能を追加

--- TKChords.swift
+++ TKChords.swift
@@ -31,15 +31,66 @@ class TKChords : TKNotesProtocol {
     }
     
     public static func from(_ scale : TKScale.NaturalMinor) -> [String] {
-        return []
+        
+        var chords : [String] = []
+        let notes = TKNotes.from(scale)
+        
+        if (notes.count == 7) {
+        
+            chords.append(notes[0] + "m")
+            chords.append(notes[1] + "m7-5")
+            chords.append(notes[2])
+            chords.append(notes[3] + "m")
+            chords.append(notes[4] + "m")
+            chords.append(notes[5])
+            chords.append(notes[6])
+            
+        }
+        
+        return chords
+        
     }
     
     public static func from(_ scale : TKScale.HarmonicMinor) -> [String] {
-        return []
+        
+        var chords : [String] = []
+        let notes = TKNotes.from(scale)
+        
+        if (notes.count == 7) {
+        
+            chords.append(notes[0] + "m")
+            chords.append(notes[1] + "m7-5")
+            chords.append(notes[2] + "aug")
+            chords.append(notes[3] + "m")
+            chords.append(notes[4])
+            chords.append(notes[5])
+            chords.append(notes[6] + "dim")
+            
+        }
+        
+        return chords
+        
     }
     
     public static func from(_ scale : TKScale.MelodicMinor) -> [String] {
-        return []
+        
+        var chords : [String] = []
+        let notes = TKNotes.from(scale)
+        
+        if (notes.count == 7) {
+        
+            chords.append(notes[0] + "m")
+            chords.append(notes[1] + "m")
+            chords.append(notes[2] + "aug")
+            chords.append(notes[3])
+            chords.append(notes[4])
+            chords.append(notes[5] + "m7-5")
+            chords.append(notes[6] + "m7-5")
+            
+        }
+        
+        return chords
+        
     }
     
 }
