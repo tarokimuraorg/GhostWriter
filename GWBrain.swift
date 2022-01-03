72e7adad9ce83f3e17455edc8b45c06c18e43743
tarokimuraorg <tarokimuraorg@outlook.com>
2022-01-03 15:36:55 +0900

    コード進行のパターンを追加

--- GWBrain.swift
+++ GWBrain.swift
@@ -42,15 +42,78 @@ class GWBrain {
     }
     
     // major : I -> IV
-    public var pattern1 : GWSeed {
-        let seed = GWSeed(head_chord: self.major_chords[0], tail_chord: self.major_chords[3])
-        return seed
+    public var seed1 : GWSeed {
+        return GWSeed(head_chord: self.major_chords[0], tail_chord: self.major_chords[3])
     }
     
     // major : I -> V
-    public var pattern2 : GWSeed {
-        let seed = GWSeed(head_chord: self.major_chords[0], tail_chord: self.major_chords[4])
-        return seed
+    public var seed2 : GWSeed {
+        return GWSeed(head_chord: self.major_chords[0], tail_chord: self.major_chords[4])
+    }
+    
+    // major : IV -> I
+    public var seed3 : GWSeed {
+        return GWSeed(head_chord: self.major_chords[3], tail_chord: self.major_chords[0])
+    }
+    
+    // major : IV -> V
+    public var seed4 : GWSeed {
+        return GWSeed(head_chord: self.major_chords[3], tail_chord: self.major_chords[4])
+    }
+    
+    // major : V -> I
+    public var seed5 : GWSeed {
+        return GWSeed(head_chord: self.major_chords[4], tail_chord: self.major_chords[0])
+    }
+    
+    // relative natural major : I -> IV
+    public var seed6 : GWSeed {
+        return GWSeed(head_chord: self.relative_natural_minor_chords[0], tail_chord: self.relative_natural_minor_chords[3])
+    }
+    
+    // relative natural minor : I -> V
+    public var seed7 : GWSeed {
+        return GWSeed(head_chord: self.relative_natural_minor_chords[0], tail_chord: self.relative_natural_minor_chords[4])
+    }
+    
+    // relative natural minor : IV -> I
+    public var seed8 : GWSeed {
+        return GWSeed(head_chord: self.relative_natural_minor_chords[3], tail_chord: self.relative_natural_minor_chords[0])
+    }
+    
+    // relative natural minor : IV -> V
+    public var seed9 : GWSeed {
+        return GWSeed(head_chord: self.relative_natural_minor_chords[3], tail_chord: self.relative_natural_minor_chords[4])
+    }
+    
+    // relative natural minor : V -> I
+    public var seed10 : GWSeed {
+        return GWSeed(head_chord: self.relative_natural_minor_chords[4], tail_chord: self.relative_natural_minor_chords[0])
+    }
+    
+    // parallel natural major : I -> IV
+    public var seed11 : GWSeed {
+        return GWSeed(head_chord: self.parallel_natural_minor_chords[0], tail_chord: self.parallel_natural_minor_chords[3])
+    }
+    
+    // parallel natural minor : I -> V
+    public var seed12 : GWSeed {
+        return GWSeed(head_chord: self.parallel_natural_minor_chords[0], tail_chord: self.parallel_natural_minor_chords[4])
+    }
+    
+    // parallel natural minor : IV -> I
+    public var seed13 : GWSeed {
+        return GWSeed(head_chord: self.parallel_natural_minor_chords[3], tail_chord: self.parallel_natural_minor_chords[0])
+    }
+    
+    // parallel natural minor : IV -> V
+    public var seed14 : GWSeed {
+        return GWSeed(head_chord: self.parallel_natural_minor_chords[3], tail_chord: self.parallel_natural_minor_chords[4])
+    }
+    
+    // parallel natural minor : V -> I
+    public var seed15 : GWSeed {
+        return GWSeed(head_chord: self.parallel_natural_minor_chords[4], tail_chord: self.parallel_natural_minor_chords[0])
     }
     
 }
