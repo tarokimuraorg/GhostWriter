dbd8c3b5434304073a7ac93b2cf15d41915262fc
tarokimuraorg <tarokimuraorg@outlook.com>
2022-01-25 14:56:25 +0900

    関数内の機能を分解
    一つの関数から機能を取り出し、二つの関数を作成した。

--- GWBrain.swift
+++ GWBrain.swift
@@ -42,284 +42,297 @@ class GWBrain {
     }
     
     // major : I -> IV
-    public var seed1 : GWSeed {
+    private var seed1 : GWSeed {
         return GWSeed(head_chord: self.major_chords.I, tail_chord: self.major_chords.IV)
     }
     
     // major : I -> V
-    public var seed2 : GWSeed {
+    private var seed2 : GWSeed {
         return GWSeed(head_chord: self.major_chords.I, tail_chord: self.major_chords.V)
     }
     
     // major : IV -> I
-    public var seed3 : GWSeed {
+    private var seed3 : GWSeed {
         return GWSeed(head_chord: self.major_chords.IV, tail_chord: self.major_chords.I)
     }
     
     // major : IV -> V
-    public var seed4 : GWSeed {
+    private var seed4 : GWSeed {
         return GWSeed(head_chord: self.major_chords.IV, tail_chord: self.major_chords.V)
     }
     
     // major : V -> I
-    public var seed5 : GWSeed {
+    private var seed5 : GWSeed {
         return GWSeed(head_chord: self.major_chords.V, tail_chord: self.major_chords.I)
     }
     
     // relative natural major : I -> IV
-    public var seed6 : GWSeed {
+    private var seed6 : GWSeed {
         return GWSeed(head_chord: self.relative_natural_minor_chords.I, tail_chord: self.relative_natural_minor_chords.IV)
     }
     
     // relative natural minor : I -> V
-    public var seed7 : GWSeed {
+    private var seed7 : GWSeed {
         return GWSeed(head_chord: self.relative_natural_minor_chords.I, tail_chord: self.relative_natural_minor_chords.V)
     }
     
     // relative natural minor : IV -> I
-    public var seed8 : GWSeed {
+    private var seed8 : GWSeed {
         return GWSeed(head_chord: self.relative_natural_minor_chords.IV, tail_chord: self.relative_natural_minor_chords.I)
     }
     
     // relative natural minor : IV -> V
-    public var seed9 : GWSeed {
+    private var seed9 : GWSeed {
         return GWSeed(head_chord: self.relative_natural_minor_chords.IV, tail_chord: self.relative_natural_minor_chords.V)
     }
     
     // relative natural minor : V -> I
-    public var seed10 : GWSeed {
+    private var seed10 : GWSeed {
         return GWSeed(head_chord: self.relative_natural_minor_chords.V, tail_chord: self.relative_natural_minor_chords.I)
     }
     
     // parallel natural major : I -> IV
-    public var seed11 : GWSeed {
+    private var seed11 : GWSeed {
         return GWSeed(head_chord: self.parallel_natural_minor_chords.I, tail_chord: self.parallel_natural_minor_chords.IV)
     }
     
     // parallel natural minor : I -> V
-    public var seed12 : GWSeed {
+    private var seed12 : GWSeed {
         return GWSeed(head_chord: self.parallel_natural_minor_chords.I, tail_chord: self.parallel_natural_minor_chords.V)
     }
     
     // parallel natural minor : IV -> I
-    public var seed13 : GWSeed {
+    private var seed13 : GWSeed {
         return GWSeed(head_chord: self.parallel_natural_minor_chords.IV, tail_chord: self.parallel_natural_minor_chords.I)
     }
     
     // parallel natural minor : IV -> V
-    public var seed14 : GWSeed {
+    private var seed14 : GWSeed {
         return GWSeed(head_chord: self.parallel_natural_minor_chords.IV, tail_chord: self.parallel_natural_minor_chords.V)
     }
     
     // parallel natural minor : V -> I
-    public var seed15 : GWSeed {
+    private var seed15 : GWSeed {
         return GWSeed(head_chord: self.parallel_natural_minor_chords.V, tail_chord: self.parallel_natural_minor_chords.I)
     }
     
     // relative harmonic major : I -> IV
-    public var seed16 : GWSeed {
+    private var seed16 : GWSeed {
         return GWSeed(head_chord: self.relative_harmonic_minor_chords.I, tail_chord: self.relative_harmonic_minor_chords.IV)
     }
     
     // relative harmonic minor : I -> V
-    public var seed17 : GWSeed {
+    private var seed17 : GWSeed {
         return GWSeed(head_chord: self.relative_harmonic_minor_chords.I, tail_chord: self.relative_harmonic_minor_chords.V)
     }
     
     // relative harmonic minor : IV -> I
-    public var seed18 : GWSeed {
+    private var seed18 : GWSeed {
         return GWSeed(head_chord: self.relative_harmonic_minor_chords.IV, tail_chord: self.relative_harmonic_minor_chords.I)
     }
     
     // relative harmonic minor : IV -> V
-    public var seed19 : GWSeed {
+    private var seed19 : GWSeed {
         return GWSeed(head_chord: self.relative_harmonic_minor_chords.IV, tail_chord: self.relative_harmonic_minor_chords.V)
     }
     
     // relative harmonic minor : V -> I
-    public var seed20 : GWSeed {
+    private var seed20 : GWSeed {
         return GWSeed(head_chord: self.relative_harmonic_minor_chords.V, tail_chord: self.relative_harmonic_minor_chords.I)
     }
     
     // parallel harmonic major : I -> IV
-    public var seed21 : GWSeed {
+    private var seed21 : GWSeed {
         return GWSeed(head_chord: self.parallel_harmonic_minor_chords.I, tail_chord: self.parallel_harmonic_minor_chords.IV)
     }
     
     // parallel harmonic minor : I -> V
-    public var seed22 : GWSeed {
+    private var seed22 : GWSeed {
         return GWSeed(head_chord: self.parallel_harmonic_minor_chords.I, tail_chord: self.parallel_harmonic_minor_chords.V)
     }
     
     // parallel harmonic minor : IV -> I
-    public var seed23 : GWSeed {
+    private var seed23 : GWSeed {
         return GWSeed(head_chord: self.parallel_harmonic_minor_chords.IV, tail_chord: self.parallel_harmonic_minor_chords.I)
     }
     
     // parallel harmonic minor : IV -> V
-    public var seed24 : GWSeed {
+    private var seed24 : GWSeed {
         return GWSeed(head_chord: self.parallel_harmonic_minor_chords.IV, tail_chord: self.parallel_harmonic_minor_chords.V)
     }
     
     // parallel harmonic minor : V -> I
-    public var seed25 : GWSeed {
+    private var seed25 : GWSeed {
         return GWSeed(head_chord: self.parallel_harmonic_minor_chords.V, tail_chord: self.parallel_harmonic_minor_chords.I)
     }
     
     // relative melodic major : I -> IV
-    public var seed26 : GWSeed {
+    private var seed26 : GWSeed {
         return GWSeed(head_chord: self.relative_melodic_minor_chords.I, tail_chord: self.relative_melodic_minor_chords.IV)
     }
     
     // relative melodic minor : I -> V
-    public var seed27 : GWSeed {
+    private var seed27 : GWSeed {
         return GWSeed(head_chord: self.relative_melodic_minor_chords.I, tail_chord: self.relative_melodic_minor_chords.V)
     }
     
     // relative melodic minor : IV -> I
-    public var seed28 : GWSeed {
+    private var seed28 : GWSeed {
         return GWSeed(head_chord: self.relative_melodic_minor_chords.IV, tail_chord: self.relative_melodic_minor_chords.I)
     }
     
     // relative melodic minor : IV -> V
-    public var seed29 : GWSeed {
+    private var seed29 : GWSeed {
         return GWSeed(head_chord: self.relative_melodic_minor_chords.IV, tail_chord: self.relative_melodic_minor_chords.V)
     }
     
     // relative melodic minor : V -> I
-    public var seed30 : GWSeed {
+    private var seed30 : GWSeed {
         return GWSeed(head_chord: self.relative_melodic_minor_chords.V, tail_chord: self.relative_melodic_minor_chords.I)
     }
     
     // parallel melodic major : I -> IV
-    public var seed31 : GWSeed {
+    private var seed31 : GWSeed {
         return GWSeed(head_chord: self.parallel_melodic_minor_chords.I, tail_chord: self.parallel_melodic_minor_chords.IV)
     }
     
     // parallel melodic minor : I -> V
-    public var seed32 : GWSeed {
+    private var seed32 : GWSeed {
         return GWSeed(head_chord: self.parallel_melodic_minor_chords.I, tail_chord: self.parallel_melodic_minor_chords.V)
     }
     
     // parallel melodic minor : IV -> I
-    public var seed33 : GWSeed {
+    private var seed33 : GWSeed {
         return GWSeed(head_chord: self.parallel_melodic_minor_chords.IV, tail_chord: self.parallel_melodic_minor_chords.I)
     }
     
     // parallel melodic minor : IV -> V
-    public var seed34 : GWSeed {
+    private var seed34 : GWSeed {
         return GWSeed(head_chord: self.parallel_melodic_minor_chords.IV, tail_chord: self.parallel_melodic_minor_chords.V)
     }
     
     // parallel melodic minor : V -> I
-    public var seed35 : GWSeed {
+    private var seed35 : GWSeed {
         return GWSeed(head_chord: self.parallel_melodic_minor_chords.V, tail_chord: self.parallel_melodic_minor_chords.I)
     }
     
     // major : I -> III
-    public var seed36 : GWSeed {
+    private var seed36 : GWSeed {
         return GWSeed(head_chord: self.major_chords.I, tail_chord: self.major_chords.III)
     }
     
     // major : I -> VI
-    public var seed37 : GWSeed {
+    private var seed37 : GWSeed {
         return GWSeed(head_chord: self.major_chords.I, tail_chord: self.major_chords.VI)
     }
     
     // major : IV -> II
-    public var seed38 : GWSeed {
+    private var seed38 : GWSeed {
         return GWSeed(head_chord: self.major_chords.IV, tail_chord: self.major_chords.II)
     }
     
     public var genetically_modified_seeds : [GWSeed] {
         
-        var storehouse : [[GWSeed]] = [[]]
+        var gmseeds = major_tonic_convertor()
+        
+        gmseeds += major_subdominant_convertor(gmseeds)
+        
+        return gmseeds
+        
+    }
+    
+    private func major_tonic_convertor() -> [GWSeed] {
+        
         var gmseeds : [GWSeed] = []
         
         // major : I -> IV
         var seed = self.seed1
         
         // T -> IV
-        var gmseed = GWSeed(head_chord: self.major_chords.III, tail_chord: seed.tail_chord)
-        
-        gmseeds.append(gmseed)
-        gmseed.head_chord = self.major_chords.VI
-        gmseeds.append(gmseed)
-        
-        // T -> II
-        for gm in gmseeds {
-            gmseed = GWSeed(head_chord: gm.head_chord, tail_chord: self.major_chords.II)
-            gmseeds.append(gmseed)
-        }
-        
-        // I -> II
-        gmseed = GWSeed(head_chord: seed.head_chord, tail_chord: self.major_chords.II)
-        gmseeds.append(gmseed)
-        
-        storehouse.append(gmseeds)
-        gmseeds = []
+        seed.head_chord = self.major_chords.III
+        gmseeds.append(seed)
+        seed.head_chord = self.major_chords.VI
+        gmseeds.append(seed)
         
         // major : I -> V
-        gmseed = self.seed2
+        seed = self.seed2
         
         // T -> V
-        gmseed.head_chord = self.major_chords.III
-        gmseeds.append(gmseed)
-        gmseed.head_chord = self.major_chords.VI
-        gmseeds.append(gmseed)
-        
-        storehouse.append(gmseeds)
-        gmseeds = []
+        seed.head_chord = self.major_chords.III
+        gmseeds.append(seed)
+        seed.head_chord = self.major_chords.VI
+        gmseeds.append(seed)
         
         // major : IV -> I
         seed = self.seed3
         
         // IV -> T
-        gmseed = GWSeed(head_chord: seed.head_chord, tail_chord: self.major_chords.III)
-        gmseeds.append(gmseed)
-        gmseed.tail_chord = self.major_chords.VI
-        gmseeds.append(gmseed)
-        
-        // II -> T
-        for gm in gmseeds {
-            gmseed = GWSeed(head_chord: self.major_chords.II, tail_chord: gm.tail_chord)
-            gmseeds.append(gmseed)
-        }
-        
-        // II -> I
-        gmseed = GWSeed(head_chord: self.major_chords.II, tail_chord: seed.tail_chord)
-        gmseeds.append(gmseed)
-        
-        storehouse.append(gmseeds)
-        gmseeds = []
-        
-        // major : IV -> V
-        gmseed = self.seed4
-        
-        // II -> V
-        gmseed.head_chord = self.major_chords.II
-        gmseeds.append(gmseed)
-        
-        storehouse.append(gmseeds)
-        gmseeds = []
+        seed.tail_chord = self.major_chords.III
+        gmseeds.append(seed)
+        seed.tail_chord = self.major_chords.VI
+        gmseeds.append(seed)
         
         // major : V -> I
-        gmseed = self.seed5
+        seed = self.seed5
         
         // V -> T
-        gmseed.tail_chord = self.major_chords.III
-        gmseeds.append(gmseed)
-        gmseed.tail_chord = self.major_chords.VI
-        gmseeds.append(gmseed)
-        
-        storehouse.append(gmseeds)
-        gmseeds = []
-        
-        for gms in storehouse {
-            gmseeds += gms
-        }
+        seed.tail_chord = self.major_chords.III
+        gmseeds.append(seed)
+        seed.tail_chord = self.major_chords.VI
+        gmseeds.append(seed)
         
         return gmseeds
+        
+    }
+    
+    private func major_subdominant_convertor(_ gmseeds : [GWSeed]) -> [GWSeed] {
+        
+        var outGMSeeds : [GWSeed] = []
+        
+        // major : T -> IV
+        var inGMSeeds = gmseeds.filter{
+            ($0.head_chord == self.major_chords.III && $0.tail_chord == self.major_chords.IV)
+            || ($0.head_chord == self.major_chords.VI && $0.tail_chord == self.major_chords.IV)
+        }
+        
+        // T -> II
+        for gmseed in inGMSeeds {
+            outGMSeeds.append(GWSeed(head_chord: gmseed.head_chord, tail_chord: self.major_chords.II))
+        }
+        
+        // major : I -> IV
+        var seed = self.seed1
+        
+        // I -> II
+        seed.tail_chord = self.major_chords.II
+        outGMSeeds.append(seed)
+        
+        // major : IV -> T
+        inGMSeeds = gmseeds.filter{
+            ($0.head_chord == self.major_chords.IV && $0.tail_chord == self.major_chords.III)
+            || ($0.head_chord == self.major_chords.IV && $0.tail_chord == self.major_chords.VI)
+        }
+        
+        // II -> T
+        for gmseed in inGMSeeds {
+            outGMSeeds.append(GWSeed(head_chord: self.major_chords.II, tail_chord: gmseed.tail_chord))
+        }
+        
+        // major : IV -> I
+        seed = self.seed3
+        
+        // II -> I
+        seed.head_chord = self.major_chords.II
+        outGMSeeds.append(seed)
+        
+        // major : IV -> V
+        seed = self.seed4
+        
+        // II -> V
+        seed.head_chord = self.major_chords.II
+        outGMSeeds.append(seed)
+        
+        return outGMSeeds
         
     }
     
